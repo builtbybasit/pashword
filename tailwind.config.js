6573db5a7679a22f7e791a74377d3b118d7e799e
Abdul Basit Rana <abranasays@gmail.com>
2026-05-27 16:01:23 +0500

    feat: upgrade nextjs 15 -> 16, tailwindcss 3 -> 4 & fix opennext errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,63 +1,27 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
   theme: {
-    screens: {
-      xxs: "340px",
-      xs: "425px",
-      ...defaultTheme.screens,
-    },
     extend: {
       animation: {
         bg: "bg 50s infinite ease",
         blob: "blob 25s infinite ease",
         blob2: "blob 15s infinite ease",
-        blob3: "blob 7s infinite",
+        blob3: "blob3 7s infinite",
         updown: "updown 2s infinite ease",
       },
-      width: {
-        128: "32rem",
-      },
-      height: {
-        128: "32rem",
-      },
-      colors: {
-        slate: {
-          1000: "#0b061d",
-        },
-      },
-      blur: {
-        "4xl": "72px",
-        "5xl": "84px",
-        "6xl": "100px",
-      },
+
       keyframes: {
         blob: {
-          "0%": {
-            backgroundPosition: "0%",
-          },
-          "50%": {
-            backgroundPosition: "100%",
-          },
-          "100%": {
-            backgroundPosition: "0%",
-          },
+          "0%": { backgroundPosition: "0%" },
+          "50%": { backgroundPosition: "100%" },
+          "100%": { backgroundPosition: "0%" },
         },
+
         bg: {
-          "0%": {
-            backgroundPosition: "0%",
-          },
-          "50%": {
-            backgroundPosition: "100%",
-          },
-          "100%": {
-            backgroundPosition: "0%",
-          },
+          "0%": { backgroundPosition: "0%" },
+          "50%": { backgroundPosition: "100%" },
+          "100%": { backgroundPosition: "0%" },
         },
+
         updown: {
           "0%": {
             transform: "translateY(0)",
@@ -71,5 +35,4 @@ module.exports = {
       },
     },
   },
-  plugins: [],
 };
